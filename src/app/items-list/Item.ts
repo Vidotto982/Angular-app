d227fc0fcfee767193e4fcf890219401fef8f2a4
Francisco Vidotto <vidotto982@gmail.com>
2021-05-24 22:10:37 -0300

    ngClass and piper

---
+++ src/app/items-list/Item.ts
@@ -0,0 +1,7 @@
+export interface Item {
+    nombre: string;
+    clase: string;
+    modelo: string;
+    precio: number;
+    oferta: boolean;
+}
--- src/app/items-list/items-list.component.ts
+++ src/app/items-list/items-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from './Item';
 
 @Component({
   selector: 'app-items-list',
@@ -7,17 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemsListComponent implements OnInit {
 
-  items = [{
+  items : Item []= [{
     nombre: 'Rueda algo',
     clase: 'Epic',
     modelo: 'Torino',
     precio: 42,
+    oferta: true,
   },
   {
     nombre: 'Chasis algo',
     clase: 'Mistic',
     modelo: 'BMW',
     precio: 77,
+    oferta: false,
+
+  },
+  {
+    nombre: 'Ruedas',
+    clase: 'Legend',
+    modelo: 'Ferrari',
+    precio: 42,
+    oferta: true,
+
+  },
+  {
+    nombre: 'Chasis algo',
+    clase: 'Mistic',
+    modelo: 'BMW',
+    precio: 77,
+    oferta: false,
+
   }
   ]
 
